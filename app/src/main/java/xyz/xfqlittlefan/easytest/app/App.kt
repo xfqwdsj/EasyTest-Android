be7ad1824bb3d00829643e29e7825067c1613fe1
xfqwdsj <xfqwdsj@qq.com>
2021-08-13 17:32:15 +0800

    引入Compose

--- app/src/main/java/xyz/xfqlittlefan/easytest/app/App.kt
+++ app/src/main/java/xyz/xfqlittlefan/easytest/app/App.kt
@@ -12,12 +12,7 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        LeanCloud.initialize(
-            this,
-            "1KbhNSxDBKNOvAiGQEJqPE6B-gzGzoHsz",
-            "aWxGGJwdeWE7rmxyLWIbkuyT",
-            "https://1kbhnsxd.lc-cn-n1-shared.com"
-        )
+        LeanCloud.initializeSecurely(this, "1KbhNSxDBKNOvAiGQEJqPE6B-gzGzoHsz", "https://1kbhnsxd.lc-cn-n1-shared.com")
         LitePal.initialize(this)
         MyClass.init(this)
 
